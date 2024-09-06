f2e362db8b588d81307fae927029ffbec87a5f60
Marco Di Summa <mdisumma@yahoo.com>
2024-09-06 10:42:00 +0100

    test 19

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
-            <im
+            <Image
               src="/vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
